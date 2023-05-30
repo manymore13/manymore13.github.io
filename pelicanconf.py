3ecc853cd7d98c439b45eebe7637ef27988c5a36
e-Zhiwei.Yan <e-Zhiwei.Yan@geely.com>
2023-05-30 15:19:41 +0800

    链接更新

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,14 +16,11 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Github', 'https://github.com/manymore13'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
 
