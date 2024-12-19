3d3fb49fbd4f23eb297014d621d522666be03ac8
manymore13 <manymore13@qq.com>
2024-12-19 10:08:54 +0000

    修改

--- main.sh
+++ main.sh
@@ -2,8 +2,12 @@ pwd
 python -m pip install "pelican[markdown]"
 
 git clone https://github.com/manymore13/manymore13.github.io.git
-git checkout main
-rm -r /docs/*
-cp -r ./manymore13.github.io/* /content/
+git checkout gh-pages
+rm -r docs/*
+rm -r content/*
+cp -r ./manymore13.github.io/* ./content/
 pelican content
+git add .
+git commit -m "update notes"
+git push origin gh-pages
 
