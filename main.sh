305078322b6e0f7bdfa4645651212ab6d1f40db7
manymore13 <manymore13@qq.com>
2024-12-19 10:01:32 +0000

    修改

---
+++ main.sh
@@ -0,0 +1,8 @@
+pwd
+python -m pip install "pelican[markdown]"
+
+git clone https://github.com/manymore13/manymore13.github.io.git
+git checkout main
+rm -r /docs/*
+cp -r ./manymore13.github.io/* /content/
+pelican content
