993004836416f734f003b0178ee7ccf4d20e7da4
manymore13 <manymore13@qq.com>
2024-12-20 02:03:49 +0000

    update

--- update_notes.sh
+++ update_notes.sh
@@ -7,9 +7,6 @@ set -e
 python -m pip install --upgrade pip
 python -m pip install "pelican[markdown]"
 
-# 克隆 GitHub Pages 分支
-# git clone https://github.com/manymore13/manymore13.github.io.git
-
 #备份
 rm -rf ../temp
 mkdir -p ../temp
@@ -33,9 +30,9 @@ rm -rf ../temp
 
 # 提交和推送更改
 echo "------------------"
-# git branch
-# git config --global user.name "action"
-# git config --global user.email "action@qq.com"
-# git add .
-# git commit -m "Automated update notes" || echo "No changes to commit"
-# git push origin gh-pages
+git branch
+git config --global user.name "action"
+git config --global user.email "action@qq.com"
+git add .
+git commit -m "Automated update notes" || echo "No changes to commit"
+git push origin gh-pages
